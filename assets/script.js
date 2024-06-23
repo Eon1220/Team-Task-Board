7148ce6a68d8e7d387c74b4872bc1b4a5a46f3d5
Eon Pace <nicolaspace21@gmail.com>
2024-06-23 14:29:45 -0400

    adds modal

--- assets/script.js
+++ assets/script.js
@@ -19,20 +19,7 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+   
 
 }
 
-// Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
-
-// Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
-
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
-
-});
