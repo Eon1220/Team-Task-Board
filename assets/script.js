fdc26858e818be6bee61f7f9c1c94af9914fa6f8
Eon Pace <nicolaspace21@gmail.com>
2024-06-23 23:45:37 -0400

    Improved modal, Unable to store or print from local storage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,10 @@
+
+const taskDisplayEl = $('#task-display');
+const taskFormEl = $('#task-form');
+const taskNameInputEl = $('#task-name-input');
+const taskDateInputEl = $('#taskDueDate');
+const taskDescrpInputEl = $('#taskDescrp');
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
@@ -5,21 +12,140 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
+    let taskList = JSON.parse(localStorage.getItem("tasks"));
+
+    if (!taskList) {
+        taskList = [];
+    }
+
+    return taskList;
+}
+
+function saveTasksToStorage(taskList) {
+    localStorage.setItem('tasks', JSON.stringify(taskList));
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskCard = $('<div>')
+    .addClass('card task-card draggable my-3')
+    .attr('data-task-id', task.id);
+  const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+  const cardBody = $('<div>').addClass('card-body');
+  const cardDescription = $('<p>').addClass('card-text').text(task.type);
+  const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+  const cardDeleteBtn = $('<button>')
+    .addClass('btn btn-danger delete')
+    .text('Delete')
+    .attr('data-task-id', task.id);
+  cardDeleteBtn.on('click', handleDeletetask);
 
+  // ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
+  if (task.dueDate && task.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+
+    // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+    if (now.isSame(taskDueDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(taskDueDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
+    }
+  }
+
+  // ? Gather all the elements created above and append them to the correct elements.
+  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  // ? Return the card so it can be appended to the correct lane.
+  return taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    const tasks = readTasksFromStorage();
 
+    // ? Empty existing task cards out of the lanes
+    const todo = $('#todo-cards');
+    todoList.empty();
+  
+    const inProgressList = $('#in-progress-cards');
+    inProgressList.empty();
+  
+    const doneList = $('#done-cards');
+    doneList.empty();
+  
+    // ? Loop through tasks and create task cards for each status
+    for (let task of tasks) {
+      if (task.status === 'to-do') {
+        todoList.append(createtaskCard(task));
+      } else if (task.status === 'in-progress') {
+        inProgressList.append(createtaskCard(task));
+      } else if (task.status === 'done') {
+        doneList.append(createtaskCard(task));
+      }
+    }
+  
+    // ? Use JQuery UI to make task cards draggable
+    $('.draggable').draggable({
+      opacity: 0.7,
+      zIndex: 100,
+      // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+      helper: function (e) {
+        // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+        const original = $(e.target).hasClass('ui-draggable')
+          ? $(e.target)
+          : $(e.target).closest('.ui-draggable');
+        // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+        return original.clone().css({
+          width: original.outerWidth(),
+        });
+      },
+    });
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
-   
+    event.preventDefault();
+
+    // ? Read user input from the form
+    const taskName = taskNameInputEl.val().trim();
+    const taskDate = taskDateInputEl.val(); // yyyy-mm-dd format
+  
+    const newtask = {
+      // ? Here we use a Web API called `crypto` to generate a random id for our task. This is a unique identifier that we can use to find the task in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.    id: crypto.randomUUID(),
+      name: taskName,
+      dueDate: taskDate,
+      status: 'to-do',
+    };
+  
+    // ? Pull the tasks from localStorage and push the new task to the array
+    const tasks = readTasksFromStorage();
+    tasks.push(newtask);
+  
+    // ? Save the updated tasks array to localStorage
+    savetasksToStorage(tasks);
+  
+    // ? Print task data back to the screen
+    printtaskData();
+  
+    // ? Clear the form inputs
+    taskNameInputEl.val('');
+    taskDateInputEl.val('');
+}
+
+// Todo: create a function to handle deleting a task
+function handleDeleteTask(event){
 
 }
 
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+
+}
+
+// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {
+ 
+});
